0ae298d3bd7a592a51ec1cdfeb4e9678b0715562
mattia-lavello <mattia.lavello@aldini.istruzioneer.it>
2025-11-12 10:51:52 +0000

    1211 vettori generici

--- ADT/src/App.java
+++ ADT/src/App.java
@@ -1,8 +1,13 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        Vettore x = new Vettore(100, 50);
-        for (int i=0; i<2000; i++)
-            x.add("X" + i);
+        Vettore<String> x = new Vettore<String>(0, 0); //classe generica
+        for (int i=0; i<23; i++)
+            x.add("Juve_" + i);
         System.out.println(x);
+        
+        System.out.println(x.get(3));
+        System.out.println(x.get(3).getClass());
+
+        Vettore<Persona> elenco = new Vettore<Persona>(10, 5);
     }
 }
--- ADT/src/Vettore.java
+++ ADT/src/Vettore.java
@@ -1,22 +1,35 @@
-public class Vettore {
+public class Vettore<T> { //abbiamo creato una classe generica, cioè una classe che ha come parmaetro un'altra classe
 
-    String[] v;
-    int n, delta;
+    T[] v;
+    int n, start, delta;
 
-    public Vettore(int dim, int delta) {
-        v = new String[dim];
+    @SuppressWarnings("unchecked")
+
+    public Vettore(int start, int delta) {
+        if(start<=0) start=10;
+        if(delta<=0) delta=10;
+        v = (T[]) new Object[start];
+        this.start = start;
         this.delta = delta;
         n = 0;
     }
 
-    public void add(String s) {
-        if (n>=v.length) {
-            String[] v2 = new String[v.length + delta];
+    @SuppressWarnings("unchecked")
+
+    public void add(T s) {
+        if (v.length==n) {
+            T[] v2 = (T[]) new Object[v.length + delta];
             for (int i=0; i<n; i++) v2[i]=v[i];
             v = v2;
         }
         v[n++] = s;
     }
+
+    public T get(int i) {
+        if (i<0 || i>v.length) return null;
+            return v[i];
+    }
+
 
     public String toString() {
         String s = "Sono dimensionato a " + v.length + ", ma contengo " + n + " valori, che sono questi:\n";
